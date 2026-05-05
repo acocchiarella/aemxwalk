2e8d238f0a93beaf797fc7a0b583a73f8ccb09ee
acocchiarella <a.cocchiarella@gmail.com>
2026-05-05 14:29:59 +1000

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,4 @@
-// add delayed functionality here
-/* === Adobe Web SDK (Target) – EDS safe === */
+/* Adobe Target – Edge Delivery safe */
 
 function alloyQueue(...args) {
   (window.alloy.q = window.alloy.q || []).push(args);
@@ -7,21 +6,22 @@ function alloyQueue(...args) {
 
 window.alloy = alloyQueue;
 
-const alloyScript = document.createElement('script');
-alloyScript.src = 'https://cdn1.adoberesources.net/alloy/2.14.0/alloy.min.js';
-alloyScript.async = true;
+window.addEventListener('load', () => {
+  const s = document.createElement('script');
+  s.src = 'https://cdn1.adoberesources.net/alloy/2.14.0/alloy.min.js';
+  s.async = true;
 
-alloyScript.onload = () => {
-  window.alloy('configure', {
-    edgeConfigId: '78186df0-137a-4df7-9ba5-831a9a646847',
-    orgId: 'O8F361935D7FA1A0A495FCF@AdobeOrg',
-    debugEnabled: true,
-  });
+  s.onload = () => {
+    window.alloy('configure', {
+      edgeConfigId: '78186df0-137a-4df7-9ba5-831a9a646847',
+      orgId: 'O8F361935D7FA1A0A495FCF@AdobeOrg',
+    });
 
-  window.alloy('sendEvent', {
-    decisionScopes: ['__view__'],
-    renderDecisions: true,
-  });
-};
+    window.alloy('sendEvent', {
+      decisionScopes: ['__view__'],
+      renderDecisions: true,
+    });
+  };
 
-document.head.appendChild(alloyScript);
+  document.head.appendChild(s);
+});
