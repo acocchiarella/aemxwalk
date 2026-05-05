66227799a682c1b22f550d869b7a731e263ddc3e
acocchiarella <a.cocchiarella@gmail.com>
2026-05-05 13:53:46 +1000

    Add Adobe Web SDK delayed functionality

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,27 @@
 // add delayed functionality here
+/* === Adobe Web SDK (Target) – EDS safe === */
+
+function alloyQueue(...args) {
+  (window.alloy.q = window.alloy.q || []).push(args);
+}
+
+window.alloy = alloyQueue;
+
+const alloyScript = document.createElement('script');
+alloyScript.src = 'https://cdn1.adoberesources.net/alloy/2.14.0/alloy.min.js';
+alloyScript.async = true;
+
+alloyScript.onload = () => {
+  window.alloy('configure', {
+    edgeConfigId: '78186df0-137a-4df7-9ba5-831a9a646847',
+    orgId: 'O8F361935D7FA1A0A495FCF@AdobeOrg',
+    debugEnabled: true,
+  });
+
+  window.alloy('sendEvent', {
+    decisionScopes: ['__view__'],
+    renderDecisions: true,
+  });
+};
+
+document.head.appendChild(alloyScript);
